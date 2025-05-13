c1286fe93088e62ed777701855dddbacf434a2ba
BogdanZhdanovich <bogzhdan44@gmail.com>
2025-05-13 23:20:10 +0300

    add models

--- service/models.py
+++ service/models.py
@@ -7,7 +7,7 @@ class Service(models.Model):
     title = models.CharField(max_length=200, verbose_name="Название услуги")
     description = models.TextField(verbose_name="Описание услуги")
     category = models.CharField(max_length=100, verbose_name="Категория")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена", default=0)# Поле "Цена"
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена", default=0)
     provider = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Поставщик услуги")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     location = models.CharField(max_length=100, default='', verbose_name="Город")
